c8b2e7aa235f5dc3d9fc950b73fb529602da9f04
DiegoRamJ <diego.ramirez.jove@gmail.com>
2019-10-02 18:06:52 -0400

    Reworked funcC.py file

--- exercise/funcC.py
+++ exercise/funcC.py
@@ -1,9 +1,21 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 def funcC(nombres):
-   return ['pedro', 'lucia'] #completar
+    notas = list(nombres.values())
+    promedio = 0
+    aprobados = []
+    for i in notas:
+        promedio += i
+    promedio = promedio/(len(notas))
+
+    for i in nombres:
+        if nombres[i]>= promedio:
+            aprobados.append(str(i))
+
+    print(aprobados)
 
 
 if __name__ == '__main__':
-    print("hello")
+    nombres = {'Diego': 100, 'Gustavo': 49, 'Limber': 100, 'Angela':100}
+    funcC(nombres)
