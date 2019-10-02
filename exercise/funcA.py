0908ece73457c9a0e2fd89392819b64772c37656
GustaveAFP <56035422+GustaveAFP@users.noreply.github.com>
2019-10-02 19:12:45 -0400

    Update funcA.py

--- exercise/funcA.py
+++ exercise/funcA.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
->>> a=input("Introduzca a");
->>> b=input("Introduzca b");
->>> c=input("Introduzca c");
+# a=input("Introduzca a");
+# b=input("Introduzca b");
+# c=input("Introduzca c");
 def funcA(a ,b, c):
 
-   print ((a+b+c)//3)
+   return ((a+b+c)//3)
    
 if __name__ == '__main__':
 
     a = 4
     b = 10
     c = 6
-funcA(a,b,c)
+print(funcA(a,b,c))
