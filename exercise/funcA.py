d32fa4062526a39203a9a9b116de2ffdfb2e1984
Arturo Hernandez <ahernandez@agetic.gob.bo>
2019-10-02 19:21:05 -0400

    cambios de gustavo

--- exercise/funcA.py
+++ exercise/funcA.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-
+# a=input("Introduzca a");
+# b=input("Introduzca b");
+# c=input("Introduzca c");
 def funcA(a ,b, c):
    prom = int((a+b+c)/3)
    return prom
-
-
-
+   
 if __name__ == '__main__':
-    print(prom)
+
+    a = 4
+    b = 10
+    c = 6
+print(funcA(a,b,c))
+
