0e9f05952095993c5b69543badbd3b4f84032913
Diego Ramírez <56008076+korvec@users.noreply.github.com>
2019-10-03 00:12:28 -0400

    Update funcC.py

--- exercise/funcC.py
+++ exercise/funcC.py
@@ -5,10 +5,7 @@ def funcC(nombres):
     notas = list(nombres.values())
     promedio = sum(notas)/(len(notas))
     ordenado = [[nombres[i],i] for i in nombres if nombres[i]>=promedio]
-
     ordenado.sort()
-    ordenado.reverse()
-
     aprobados = [ordenado[i][1] for i in range(len(ordenado))]
 
     return aprobados
