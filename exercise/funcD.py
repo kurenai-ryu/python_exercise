ee194d53615214a028651afc38767f638adb7bc6
Limber Maldonado <limber.mcas@gmail.com>
2019-10-03 09:56:56 -0400

    Adicion de docstrings

--- exercise/funcD.py
+++ exercise/funcD.py
@@ -3,7 +3,21 @@
 
 
 def funcD(cadena):
+   """Invertir cadenas o listas es posible utilizar el operador slice
+   NombreDeCadena [inicio : fin : paso]
+   
+   Que permite extraer los elementos de NombreDeCadena
+   * Cuando no se tiene inicio comienza en el primer elemento de NombreDeCadena
+   
+   * Cuando no se registra un fin, terminará en el último elemento de NombreDeCadena
+   
+   * El paso negativo, permite recorrer hacia atrás, por lo que inicio y fin se invierten
+   
+   >>>funcD("Hola Mundo")
+   odnuM aloH
+   """
    return cadena[::-1]
 
 if __name__ == '__main__':
+
     print(cadena)
